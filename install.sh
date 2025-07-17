e5a336d81af0fcdd689757ab2aec806ede99eb11
Adrián Seimandi <bajosega@gmail.com>
2025-07-17 19:46:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# install.sh - Installer for Plymouth Manager
+# Copyright (C) 2020 x1unix
+# Copyright (C) 2025 piposeimandi
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 set -euo pipefail
 
 DOWNLOAD_URL="https://raw.githubusercontent.com/piposeimandi/plymouth-manager/main/plym.sh"
